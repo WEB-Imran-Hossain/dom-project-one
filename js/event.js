19abb167a8238c0f1ef5ccf7036a77268ab5f46b
Imran <webimran2021@gmail.com>
2023-08-14 18:08:31 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,12 @@
+
+document
+  .getElementById("make-red")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "red";
+  });
+
+  document
+  .getElementById("make-green")
+  .addEventListener("click", function () {
+    document.body.style.backgroundColor = "green";
+  });
