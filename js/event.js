fff98d3ae474799a1ec48cf8a0030cb8b5a82e8c
Imran <webimran2021@gmail.com>
2023-08-14 23:30:39 +0600

    added seven card with different color

--- js/event.js
+++ js/event.js
@@ -1,3 +1,4 @@
+"use strict";
 // saturday card
 // get the class ID of button
 const makeRedButton = document.getElementById("make-red");
@@ -54,6 +55,48 @@ makeAquaButton.addEventListener("click", function () {
   cardFour.style.backgroundColor = "#00FFFF";
 });
 
+// Wednesday card
+// get the class ID of button
+const makeCeriseButton = document.getElementById("make-cerise");
+const makeCharcoalButton = document.getElementById("make-charcoal");
+const cardFiveBg = document.getElementById("cardFive");
+// get the specify card only
+makeCeriseButton.addEventListener("click", function () {
+  cardFive.style.backgroundColor = "#de3163";
+});
+
+makeCharcoalButton.addEventListener("click", function () {
+  cardFive.style.backgroundColor = "#36454f";
+});
+
+// Thursday card
+// get the class ID of button
+const makeCitronButton = document.getElementById("make-citron");
+const makeCitrineButton = document.getElementById("make-citrine");
+const cardSixBg = document.getElementById("cardsix");
+// get the specify card only
+makeCitronButton.addEventListener("click", function () {
+  cardSix.style.backgroundColor = "#9fa91f";
+});
+
+makeCitrineButton.addEventListener("click", function () {
+  cardSix.style.backgroundColor = "#e4d00a";
+});
+
+// Friday card
+// get the class ID of button
+const makeCoquelicotButton = document.getElementById("make-coquelicot");
+const makeCoralButton = document.getElementById("make-coral");
+const cardSevenBg = document.getElementById("cardSeven");
+// get the specify card only
+makeCoquelicotButton.addEventListener("click", function () {
+  cardSeven.style.backgroundColor = "	#ff3800";
+});
+
+makeCoralButton.addEventListener("click", function () {
+  cardSeven.style.backgroundColor = "#ff7f50";
+});
+
 // reset button
 const clearButton = document.getElementById("clear-btn");
 clearButton.addEventListener("click", function () {
@@ -61,4 +104,7 @@ clearButton.addEventListener("click", function () {
   cardTwo.style.backgroundColor = "";
   cardThree.style.backgroundColor = "";
   cardFour.style.backgroundColor = "";
+  cardFive.style.backgroundColor = "";
+  cardSix.style.backgroundColor = "";
+  cardSeven.style.backgroundColor = "";
 });
