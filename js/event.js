254b430552c43289d62ed69b6c53732218d9f653
Imran <webimran2021@gmail.com>
2023-08-15 02:22:40 +0600

    final working day status

--- js/event.js
+++ js/event.js
@@ -2,15 +2,19 @@
 // saturday card
 // get the class ID of button
 const makeRedButton = document.getElementById("make-red");
-const makeGreenButton = document.getElementById("make-green");
-// get the specify card only
-const card = document.querySelector(".card");
+const makeHarlequinButton = document.getElementById("make-harlequin");
+const cardOne = document.getElementById("cardOne");
+const paragraphOne = document.getElementById("paragraphOne");
+// get the background color
 makeRedButton.addEventListener("click", function () {
-  card.style.backgroundColor = "#FF0000";
+  cardOne.style.backgroundColor = "#FF0000";
+  paragraphOne.textContent = "Today is a public holiday. No busy schedule.";
 });
 
-makeGreenButton.addEventListener("click", function () {
-  card.style.backgroundColor = "#008000";
+makeHarlequinButton.addEventListener("click", function () {
+  cardOne.style.backgroundColor = "#3fff00";
+  paragraphOne.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 // sunday card
@@ -18,13 +22,18 @@ makeGreenButton.addEventListener("click", function () {
 const makeOrangeButton = document.getElementById("make-orange");
 const makeYellowButton = document.getElementById("make-yellow");
 const cardTwoBg = document.getElementById("cardTwo");
-// get the specify card only
+const paragraphTwo = document.getElementById("paragraphTwo");
+// get the background color
 makeOrangeButton.addEventListener("click", function () {
   cardTwo.style.backgroundColor = "#FFA500";
+  paragraphTwo.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 makeYellowButton.addEventListener("click", function () {
   cardTwo.style.backgroundColor = "#FFFF00";
+  paragraphTwo.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 // monday card
@@ -32,13 +41,18 @@ makeYellowButton.addEventListener("click", function () {
 const makePinkButton = document.getElementById("make-pink");
 const makeChartreuseButton = document.getElementById("make-chartreuse");
 const cardThreeBg = document.getElementById("cardThree");
-// get the specify card only
+const paragraphThree = document.getElementById("paragraphThree");
+// get the background color
 makePinkButton.addEventListener("click", function () {
   cardThree.style.backgroundColor = "#FFC0CB";
+  paragraphThree.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 makeChartreuseButton.addEventListener("click", function () {
   cardThree.style.backgroundColor = "#DFFF00";
+  paragraphThree.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 // Tuesday card
@@ -46,13 +60,18 @@ makeChartreuseButton.addEventListener("click", function () {
 const makeTealButton = document.getElementById("make-teal");
 const makeAquaButton = document.getElementById("make-aqua");
 const cardFourBg = document.getElementById("cardFour");
-// get the specify card only
+const paragraphFour = document.getElementById("paragraphFour");
+// get the background color
 makeTealButton.addEventListener("click", function () {
   cardFour.style.backgroundColor = "#008080";
+  paragraphFour.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 makeAquaButton.addEventListener("click", function () {
   cardFour.style.backgroundColor = "#00FFFF";
+  paragraphFour.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 // Wednesday card
@@ -60,27 +79,37 @@ makeAquaButton.addEventListener("click", function () {
 const makeCeriseButton = document.getElementById("make-cerise");
 const makeCharcoalButton = document.getElementById("make-charcoal");
 const cardFiveBg = document.getElementById("cardFive");
-// get the specify card only
+const paragraphFive = document.getElementById("paragraphFive");
+// get the background color
 makeCeriseButton.addEventListener("click", function () {
   cardFive.style.backgroundColor = "#de3163";
+  paragraphFive.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 makeCharcoalButton.addEventListener("click", function () {
   cardFive.style.backgroundColor = "#36454f";
+  paragraphFive.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 // Thursday card
 // get the class ID of button
 const makeCitronButton = document.getElementById("make-citron");
 const makeCitrineButton = document.getElementById("make-citrine");
-const cardSixBg = document.getElementById("cardsix");
-// get the specify card only
+const cardSixBg = document.getElementById("cardSix");
+const paragraphSix = document.getElementById("paragraphSix");
+// get the background color
 makeCitronButton.addEventListener("click", function () {
   cardSix.style.backgroundColor = "#9fa91f";
+  paragraphSix.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 makeCitrineButton.addEventListener("click", function () {
   cardSix.style.backgroundColor = "#e4d00a";
+  paragraphSix.textContent =
+    "There is no holiday today. Must go to office on time.";
 });
 
 // Friday card
@@ -88,23 +117,33 @@ makeCitrineButton.addEventListener("click", function () {
 const makeCoquelicotButton = document.getElementById("make-coquelicot");
 const makeCoralButton = document.getElementById("make-coral");
 const cardSevenBg = document.getElementById("cardSeven");
-// get the specify card only
+const paragraphSeven = document.getElementById("paragraphSeven");
+// get the background color
 makeCoquelicotButton.addEventListener("click", function () {
   cardSeven.style.backgroundColor = "	#ff3800";
+  paragraphSeven.textContent = "There is holiday today. No schedule Day.";
 });
 
 makeCoralButton.addEventListener("click", function () {
   cardSeven.style.backgroundColor = "#ff7f50";
+  paragraphSeven.textContent = "There is holiday today. No schedule Day.";
 });
 
 // reset button
 const clearButton = document.getElementById("clear-btn");
 clearButton.addEventListener("click", function () {
-  card.style.backgroundColor = "";
+  cardOne.style.backgroundColor = "";
+  paragraphOne.textContent = "";
   cardTwo.style.backgroundColor = "";
+  paragraphTwo.textContent = "";
   cardThree.style.backgroundColor = "";
+  paragraphThree.textContent = "";
   cardFour.style.backgroundColor = "";
+  paragraphFour.textContent = "";
   cardFive.style.backgroundColor = "";
+  paragraphFive.textContent = "";
   cardSix.style.backgroundColor = "";
+  paragraphSix.textContent = "";
   cardSeven.style.backgroundColor = "";
+  paragraphSeven.textContent = "";
 });
