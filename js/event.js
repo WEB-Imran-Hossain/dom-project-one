b234d0dafd76cfd8ff9dc2ab995ea83df5624f9f
Imran <webimran2021@gmail.com>
2023-08-14 22:25:02 +0600

    added card two

--- js/event.js
+++ js/event.js
@@ -1,12 +1,34 @@
+// saturday card
+// get the class ID of button
+const makeRedButton = document.getElementById("make-red");
+const makeGreenButton = document.getElementById("make-green");
+// get the specify card only
+const card = document.querySelector(".card");
+makeRedButton.addEventListener("click", function () {
+  card.style.backgroundColor = "#FF0000";
+});
 
-document
-  .getElementById("make-red")
-  .addEventListener("click", function () {
-    document.body.style.backgroundColor = "red";
-  });
+makeGreenButton.addEventListener("click", function () {
+  card.style.backgroundColor = "#008000";
+});
 
-  document
-  .getElementById("make-green")
-  .addEventListener("click", function () {
-    document.body.style.backgroundColor = "green";
-  });
+// sunday card
+// get the class ID of button
+const makeOrangeButton = document.getElementById("make-orange");
+const makeYellowButton = document.getElementById("make-yellow");
+const cardTwoBg = document.getElementById("cardTwo");
+// get the specify card only
+makeOrangeButton.addEventListener("click", function () {
+  cardTwo.style.backgroundColor = "#FFA500";
+});
+
+makeYellowButton.addEventListener("click", function () {
+  cardTwo.style.backgroundColor = "#FFFF00";
+});
+
+// reset button
+const clearButton = document.getElementById("clear-btn");
+clearButton.addEventListener("click", function () {
+  card.style.backgroundColor = "";
+  cardTwo.style.backgroundColor = "";
+});
